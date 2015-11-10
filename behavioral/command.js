24e5cd64b795eba0c100f4895c8ea6647adbe547
Srijan Mukherjee <sinjarthebeast666@gmail.com>
2015-11-10 23:25:48 +0530

    Command Pattern basic

--- behavioral/command.js
+++ behavioral/command.js
@@ -1,21 +1,35 @@
+"use strict";
+
 /**
 * Command interface definition
 */
-var ICommand = function (execute, args) {
-  this.execute = execute;
-  this.args = args;
-}
-
-var BaseCommand = function(execute, args)
-{
-  ICommand.call(this, function() {}, args);
-  var baseExecute = execute;
-  this.doWork = null;
+var _ICommand = function () {
+  this.execute = undefined;
 };
 
-BaseCommand.prototype = Object.create(ICommand.prototype);
-BaseCommand.prototype.constructor = BaseCommand;
+/**
+ * The Dispatcher command that is a wrapper over the execute
+ * method of the command which allows clients to provide a
+ * custom way to execute any command
+ */
+var _Dispatch = function(dispatch) {
+  _ICommand.call(this);
+  this.dispatch = dispatch;
+};
 
-BaseCommand.prototype.execute = function () {
-  this.baseExecute (this.doWork, this.args);
+_Dispatch.prototype = Object.create(_Dispatch.prototype);
+_Dispatch.prototype.constructor = _Dispatch;
+
+_Dispatch.prototype.execute = function(command, args = undefined)
+{
+   if(typeof (this.dispatch) == 'function' &&
+      typeof (command.execute) == 'function')
+      {
+        this.dispatch.call(this, command, args);
+      }
+};
+
+module.exports = {
+  ICommand: _ICommand,
+  Dispatch: _Dispatch
 };
